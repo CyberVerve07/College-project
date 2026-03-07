689dfd61d7cc479993a8300067673b4cf08cfc75
CyberVerve07 <adityamahajan7287@gmail.com>
2026-03-07 16:48:03 +0530

    fix: setup config.ts
    Modular architecture reorganization for src/firebase/config.ts

--- src/firebase/config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  "projectId": "studio-2992920333-1583b",
-  "appId": "1:423819614804:web:f76a97f3b5ca86292cebc1",
-  "apiKey": "AIzaSyBTdyFkpuaLQBzvyB_rWD-vi1eaFW2y5Aw",
-  "authDomain": "studio-2992920333-1583b.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "423819614804"
-};
